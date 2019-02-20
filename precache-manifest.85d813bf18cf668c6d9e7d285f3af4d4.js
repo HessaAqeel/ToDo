f22bbdacf2b759e39c7fa04f11e649ff5797691b
hessaAqeel <hessa1aqeel@gmail.com>
2019-02-20 16:05:15 +0300

    Updates

---
+++ precache-manifest.85d813bf18cf668c6d9e7d285f3af4d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2e24aea5d5a30d8b5f0",
+    "url": "/ToDo/static/css/main.4ad7f9eb.chunk.css"
+  },
+  {
+    "revision": "c2e24aea5d5a30d8b5f0",
+    "url": "/ToDo/static/js/main.c2e24aea.chunk.js"
+  },
+  {
+    "revision": "3c3b113506d4277ee49a",
+    "url": "/ToDo/static/js/runtime~main.3c3b1135.js"
+  },
+  {
+    "revision": "1449b3c0dc32a11515b3",
+    "url": "/ToDo/static/js/2.1449b3c0.chunk.js"
+  },
+  {
+    "revision": "3496eafe9154bab3df937ba1e81aa49a",
+    "url": "/ToDo/index.html"
+  }
+];
